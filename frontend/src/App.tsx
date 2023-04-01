c75f685872f958a48462c9f624697fc1f627956a
Leonid Simusev <ldsimusev@avito.ru>
2023-04-01 22:19:29 +0300

    #1: Фронтенд на CRA

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+export const App = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
+      aperiam architecto cupiditate eum excepturi fuga laborum nihil omnis
+      perferendis perspiciatis quam temporibus totam voluptas? A aliquam impedit
+      incidunt placeat ratione!
+    </p>
+  );
+};
