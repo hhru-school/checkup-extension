3fad16766be196f5c4d0a68dbdb772cdd74ba9f6
Korneev Serge <sakorneev90@gmail.com>
2023-04-04 18:30:36 +0300

    Замена символа tab на spaces

--- backend/src/main/java/ru/hh/school/checkupextension/CheckupExtensionApplication.java
+++ backend/src/main/java/ru/hh/school/checkupextension/CheckupExtensionApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CheckupExtensionApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CheckupExtensionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CheckupExtensionApplication.class, args);
+    }
 
 }
