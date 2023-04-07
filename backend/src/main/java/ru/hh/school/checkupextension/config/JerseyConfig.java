828f80a8c8988f235d1778271d9431c866402daf
evgeny <polevikovea@gmail.com>
2023-04-07 10:19:42 +0300

    Refactor jersey config

---
+++ backend/src/main/java/ru/hh/school/checkupextension/config/JerseyConfig.java
@@ -0,0 +1,15 @@
+package ru.hh.school.checkupextension.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig {
+    @Bean
+    public ResourceConfig jerseyConfig() {
+        ResourceConfig config = new ResourceConfig();
+        // config.register(TestResource.class);
+        return config;
+    }
+}
