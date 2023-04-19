24e224bd21f3963eb8ad9c06b0ea13f6b99f676e
Korneev Serge <sakorneev90@gmail.com>
2023-04-19 21:05:26 +0300

    Delete unused controllers and resources

--- backend/src/main/java/ru/hh/school/checkupextension/config/AppConfiguration.java
+++ backend/src/main/java/ru/hh/school/checkupextension/config/AppConfiguration.java
@@ -5,18 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import ru.hh.school.checkupextension.core.data.controller.ProblemResource;
-import ru.hh.school.checkupextension.core.data.controller.SubmissionResource;
-import ru.hh.school.checkupextension.core.data.controller.VerificationResource;
-import ru.hh.school.checkupextension.core.data.daoimpl.ProblemDaoImpl;
-import ru.hh.school.checkupextension.core.data.daoimpl.SubmissionDaoImpl;
-import ru.hh.school.checkupextension.core.data.daoimpl.VerificationDaoImpl;
-import ru.hh.school.checkupextension.core.data.service.ProblemService;
-import ru.hh.school.checkupextension.core.data.service.SubmissionService;
-import ru.hh.school.checkupextension.core.data.service.VerificationService;
-import ru.hh.school.checkupextension.utils.exception.mapper.AuthorizedExceptionMapper;
 import ru.hh.school.checkupextension.contest.ContestApiResource;
 import ru.hh.school.checkupextension.contest.ContestService;
+import ru.hh.school.checkupextension.core.repository.ProblemRepository;
+import ru.hh.school.checkupextension.core.repository.SubmissionRepository;
+import ru.hh.school.checkupextension.core.repository.VerificationRepository;
+import ru.hh.school.checkupextension.utils.exception.mapper.AuthorizedExceptionMapper;
 import ru.hh.school.checkupextension.utils.exception.mapper.ProblemNotFoundExceptionMapper;
 import ru.hh.school.checkupextension.utils.exception.mapper.SubmissionNotFoundExceptionMapper;
 import ru.hh.school.checkupextension.utils.exception.mapper.VerificationNotFoundExceptionMapper;
@@ -24,12 +18,9 @@ import ru.hh.school.checkupextension.utils.stub.CheckupApiStub;
 
 @Configuration
 @Import({
-    VerificationDaoImpl.class,
-    VerificationService.class,
-    ProblemDaoImpl.class,
-    ProblemService.class,
-    SubmissionDaoImpl.class,
-    SubmissionService.class,
+    VerificationRepository.class,
+    ProblemRepository.class,
+    SubmissionRepository.class,
     ContestService.class,
     CheckupApiStub.class,
 })
@@ -38,9 +29,6 @@ public class AppConfiguration {
   public ResourceConfig jerseyConfig() {
     var config = new ResourceConfig();
 
-    config.register(VerificationResource.class);
-    config.register(ProblemResource.class);
-    config.register(SubmissionResource.class);
     config.register(ContestApiResource.class);
 
     config.register(VerificationNotFoundExceptionMapper.class);
