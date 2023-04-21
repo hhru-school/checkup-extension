8ccd216191f1139fecff8403fd7abc447b527670
Korneev Serge <sakorneev90@gmail.com>
2023-04-21 09:41:17 +0300

    Improve a custom exceptional example

--- backend/src/main/java/ru/hh/school/checkupextension/utils/exception/ProblemNotFoundException.java
+++ backend/src/main/java/ru/hh/school/checkupextension/utils/exception/ProblemNotFoundException.java
@@ -1,12 +1,20 @@
 package ru.hh.school.checkupextension.utils.exception;
 
-public class ProblemNotFoundException extends RuntimeException {
-  public ProblemNotFoundException(String message) {
-    super(message);
-  }
+import ru.hh.school.checkupextension.core.data.Problem;
 
-  public ProblemNotFoundException(String message, Throwable cause) {
-    super(message, cause);
-  }
+public class ProblemNotFoundException extends RuntimeException {
+
+    public static final String PROBLEM_ID_NOT_FOUND_MESSAGE = "Problem not found with id: %s";
+    public ProblemNotFoundException(Problem problem) {
+
+    }
+
+    public ProblemNotFoundException(Long problemId) {
+        super(String.format(PROBLEM_ID_NOT_FOUND_MESSAGE, problemId));
+    }
+
+    public ProblemNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
 
--- backend/src/main/java/ru/hh/school/checkupextension/utils/exception/constant/ExceptionConstants.java
+++ backend/src/main/java/ru/hh/school/checkupextension/utils/exception/constant/ExceptionConstants.java
@@ -2,7 +2,7 @@ package ru.hh.school.checkupextension.utils.exception.constant;
 
 public class ExceptionConstants {
 
-  public static final String PROBLEM_ID_NOT_FOUND_MESSAGE = "Problem not found with id: %s";
+
   public static final String SUBMISSION_ID_NOT_FOUND_MESSAGE = "Submission not found with id: %s";
   public static final String VERIFICATION_ID_NOT_FOUND_MESSAGE = "Verification not found with id: %s";
 
