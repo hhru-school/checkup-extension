cc8ba617ccbc6eaf78cd557e6b5aee8bd8fb57c3
Korneev Serge <sakorneev90@gmail.com>
2023-05-20 11:53:49 +0300

    Ability to get problem dto for administration

--- backend/src/main/java/ru/hh/school/checkupextension/utils/mapper/admin/EditableProblemMapper.java
+++ backend/src/main/java/ru/hh/school/checkupextension/utils/mapper/admin/EditableProblemMapper.java
@@ -7,8 +7,10 @@ import ru.hh.school.checkupextension.core.data.entity.Problem;
 import ru.hh.school.checkupextension.core.data.entity.Verification;
 import ru.hh.school.checkupextension.core.data.enums.ProblemType;
 import ru.hh.school.checkupextension.utils.builder.ProblemBuilder;
+import ru.hh.school.checkupextension.utils.builder.ProblemDtoBuilder;
 import ru.hh.school.checkupextension.utils.builder.TemplateBuilder;
 import ru.hh.school.checkupextension.utils.builder.VerificationBuilder;
+import ru.hh.school.checkupextension.utils.builder.VerificationDtoBuilder;
 
 public class EditableProblemMapper {
   public static Problem toEntity(EditableProblemDto problemDto) {
@@ -29,8 +31,36 @@ public class EditableProblemMapper {
   }
 
   public static EditableProblemDto toEditableProblemDto(Problem problem) {
-    // TODO: Добавить преобразование
-    return null;
+    var type = ProblemType.getTitleBy(problem.getType());
+    var verificationDto = formatVerificationDto(problem);
+    // TODO: after completion #33
+//    var solution = problem.getSolution();
+    var template = problem.getTemplate();
+
+    // TODO: after completion #33
+    return ProblemDtoBuilder.buildProblemDto(
+        problem.getId(),
+        type,
+        problem.getTitle(),
+        problem.getDescription(),
+        problem.getContent(),
+        problem.getMaxAttempts(),
+        problem.getActive(),
+        "", // solution.getHtmlPart(),
+        "", //solution.getCssPart(),
+        "", //solution.getJsPart(),
+        template.getHtmlTemplate(),
+        template.getCssTemplate(),
+        template.getJsTemplate(),
+        verificationDto
+    );
+  }
+
+  private static List<EditableVerificationDto> formatVerificationDto(Problem problem) {
+    var verifications = problem.getVerifications();
+    return verifications.stream()
+        .map(v -> VerificationDtoBuilder.buildEditableVerificationDto(v.getId(), v.getContent()))
+        .toList();
   }
 
   private static Problem.Template extractTemplate(EditableProblemDto problemDto) {
