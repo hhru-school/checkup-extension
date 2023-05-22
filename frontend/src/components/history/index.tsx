b05e31d861a1e94b2ad79499ecafb135e59083a4
Rail Munirov <rail-ko@mail.ru>
2023-05-22 19:34:23 +0300

    implement task solution page

--- frontend/src/components/history/index.tsx
+++ frontend/src/components/history/index.tsx
@@ -1,49 +1,100 @@
 import { FC, useEffect, useState } from "react";
 import styles from "./index.module.css";
-import { Button, Col, List, Row, Skeleton, Space, Typography } from "antd";
+import {
+  Alert,
+  Button,
+  Col,
+  List,
+  Row,
+  Skeleton,
+  Space,
+  Typography,
+} from "antd";
 import { useTranslation } from "react-i18next";
 import { useAppDispatch, useAppSelector } from "../../__data__/store";
-import { fetchHistory } from "../../__data__/slices/history";
-import { useParams } from "react-router-dom";
+import { fetchHistory, actions } from "../../__data__/slices/history";
+import { SolutionShort } from "../../types";
 
-export const History: FC = () => {
+const HistorySkeleton = () => {
+  const rows = new Array(7).fill(null).map((item, index) => {
+    return (
+      <Row gutter={8} key={index}>
+        <Col span={18}>
+          <Skeleton.Input active size="large" block={true} />
+        </Col>
+        <Col span={4}>
+          <Skeleton.Button active size="large" shape="default" block={false} />
+        </Col>
+      </Row>
+    );
+  });
+
+  return (
+    <>
+      <Space direction="vertical" className={styles.space}>
+        {rows}
+      </Space>
+    </>
+  );
+};
+
+type PropsType = {
+  task: number;
+};
+// TODO: переделать дизайн, убрать кнопку выбрать и каждый item сделать активным
+export const History: FC<PropsType> = ({ task }) => {
   const { t } = useTranslation();
-  const { id } = useParams();
+  const [solutionsCopy, setSolutionsCopy] = useState<Array<SolutionShort>>([]);
   const dispatch = useAppDispatch();
   const loading = useAppSelector((store) => store.history.isLoading);
   const error = useAppSelector((store) => store.history.error);
   const solutions = useAppSelector((store) => store.history.solutions);
 
   useEffect(() => {
-    dispatch(fetchHistory(Number(id)));
-  }, [dispatch, id]);
+    dispatch(fetchHistory(task));
+  }, [dispatch, task]);
+
+  useEffect(() => {
+    if (solutions) {
+      setSolutionsCopy([
+        {
+          id: -1,
+          date: new Date().toLocaleString(),
+          status: "check",
+          title: t("history.solution.current.title"),
+        },
+        ...solutions,
+      ]);
+    }
+  }, [solutions, t]);
 
   const handleShowButtonClick = (id: number) => {
-    // TODO: update store
+    dispatch(actions.setCurrentSolution(id));
   };
 
-  if (true) {
-    return (
-      <>
-        <Row gutter={8}>
-          <Col span={18}>
-            <Skeleton.Input active size="large" block={true} />
-          </Col>
-          <Col span={4}>
-            <Skeleton.Button
-              active
-              size="large"
-              shape="default"
-              block={false}
-            />
-          </Col>
-        </Row>
-      </>
-    );
+  if (loading) {
+    return <HistorySkeleton />;
   }
 
   if (error) {
-    return null;
+    return (
+      <Alert
+        message={t("history.error.title")}
+        description={t("error.loading")}
+        type="error"
+        action={
+          <Button
+            size="small"
+            danger
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            {t("button.reload")}
+          </Button>
+        }
+      />
+    );
   }
 
   return (
@@ -54,7 +105,10 @@ export const History: FC = () => {
         }
         size="small"
         bordered
-        dataSource={solutions}
+        loading={loading}
+        dataSource={solutionsCopy}
+        locale={{ emptyText: t("history.no.data") }}
+        rowKey={(item) => item.id}
         renderItem={(item) => (
           <List.Item
             className={styles.item}
