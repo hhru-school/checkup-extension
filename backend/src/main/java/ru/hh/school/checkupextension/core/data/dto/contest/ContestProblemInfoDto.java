74cce80164dd9e42bd0bc1403a8a0448a96d901d
Korneev Serge <sakorneev90@gmail.com>
2023-06-02 11:16:53 +0300

    Add getting contests problems info

---
+++ backend/src/main/java/ru/hh/school/checkupextension/core/data/dto/contest/ContestProblemInfoDto.java
@@ -0,0 +1,27 @@
+package ru.hh.school.checkupextension.core.data.dto.contest;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ru.hh.school.checkupextension.utils.constant.ProblemDtoJsonPropertyName;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ContestProblemInfoDto {
+  public final long id;
+  public final String title;
+  public final String description;
+  public final String type;
+
+  @JsonCreator
+  public ContestProblemInfoDto(
+      @JsonProperty(ProblemDtoJsonPropertyName.ID) long id,
+      @JsonProperty(ProblemDtoJsonPropertyName.TITLE) String title,
+      @JsonProperty(ProblemDtoJsonPropertyName.DESCRIPTION) String description,
+      @JsonProperty(ProblemDtoJsonPropertyName.TYPE) String type
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.type = type;
+  }
+}
--- backend/src/main/java/ru/hh/school/checkupextension/utils/mapper/contest/ContestProblemMapper.java
+++ backend/src/main/java/ru/hh/school/checkupextension/utils/mapper/contest/ContestProblemMapper.java
@@ -1,10 +1,20 @@
 package ru.hh.school.checkupextension.utils.mapper.contest;
 
 import ru.hh.school.checkupextension.core.data.dto.contest.ContestProblemDto;
+import ru.hh.school.checkupextension.core.data.dto.contest.ContestProblemInfoDto;
 import ru.hh.school.checkupextension.core.data.entity.Problem;
 import ru.hh.school.checkupextension.core.data.enums.ProblemType;
 
 public class ContestProblemMapper {
+  public static ContestProblemInfoDto toContestProblemInfo(Problem problem) {
+    String type = ProblemType.getTitleBy(problem.getType()); // Получаем строковое представление типа задачи
+    return new ContestProblemInfoDto(
+        problem.getId(),
+        problem.getTitle(),
+        problem.getDescription(),
+        type);
+  }
+
   public static ContestProblemDto toContestProblem(Problem problem) {
     var template = problem.getTemplate();
     String type = ProblemType.getTitleBy(problem.getType()); // Получаем строковое представление типа задачи
