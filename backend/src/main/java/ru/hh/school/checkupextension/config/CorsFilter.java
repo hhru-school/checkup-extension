88ad965aea8a4295eb28a03e7b402fcbbfa8dfb6
Korneev Serge <sakorneev90@gmail.com>
2023-06-02 10:41:52 +0300

    Reformat CorsFilter

--- backend/src/main/java/ru/hh/school/checkupextension/config/CorsFilter.java
+++ backend/src/main/java/ru/hh/school/checkupextension/config/CorsFilter.java
@@ -10,18 +10,22 @@ import java.io.IOException;
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
-    @Override
-    public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-    }
+  @Override
+  public void filter(
+      ContainerRequestContext requestContext,
+      ContainerResponseContext responseContext
+  ) throws IOException {
+    responseContext.getHeaders().add(
+        "Access-Control-Allow-Origin", "*");
+    responseContext.getHeaders().add(
+        "Access-Control-Allow-Credentials", "true");
+    responseContext.getHeaders().add(
+        "Access-Control-Allow-Headers",
+        "origin, content-type, accept, authorization"
+    );
+    responseContext.getHeaders().add(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS, HEAD"
+    );
+  }
 }
