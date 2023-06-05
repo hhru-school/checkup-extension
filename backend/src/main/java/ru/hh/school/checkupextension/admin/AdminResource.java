e32f60c31a6184a435b6ccb2e3ecfdea4cf190d6
Korneev Serge <sakorneev90@gmail.com>
2023-06-05 15:24:18 +0300

    Remove wildcard import

--- backend/src/main/java/ru/hh/school/checkupextension/admin/AdminResource.java
+++ backend/src/main/java/ru/hh/school/checkupextension/admin/AdminResource.java
@@ -1,7 +1,13 @@
 package ru.hh.school.checkupextension.admin;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.CookieParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import ru.hh.school.checkupextension.core.data.dto.admin.EditableProblemDto;
 
