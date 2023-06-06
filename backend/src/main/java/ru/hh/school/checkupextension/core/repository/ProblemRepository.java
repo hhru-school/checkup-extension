7f87c4ca203dd10839a5ae51bbeb853eeec2d15d
Korneev Serge <sakorneev90@gmail.com>
2023-06-06 19:46:59 +0300

    Add method to get problems in parts with pagination metadata

--- backend/src/main/java/ru/hh/school/checkupextension/core/repository/ProblemRepository.java
+++ backend/src/main/java/ru/hh/school/checkupextension/core/repository/ProblemRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.hibernate.SessionFactory;
 import ru.hh.school.checkupextension.core.data.entity.Problem;
+import ru.hh.school.checkupextension.core.data.utils.PaginationResult;
 
 public class ProblemRepository extends GenericRepository<Problem> {
   @Inject
@@ -20,6 +21,15 @@ public class ProblemRepository extends GenericRepository<Problem> {
   @Override
   public List<Problem> getAll() {
     return getSession().createQuery("SELECT p FROM Problem p", Problem.class).getResultList();
+  }
+
+  public PaginationResult<Problem> getProblems(int firstRecord, int size) {
+    var totalRecords = getSession()
+        .createQuery("SELECT count(*) FROM Problem", Long.class)
+        .getSingleResult();
+    var totalPages = Math.ceil(totalRecords / (double) size);
+    var records = getProblemsFrom(firstRecord, size);
+    return new PaginationResult<>(firstRecord, size, (int) totalPages, totalRecords, records);
   }
 
   public List<Problem> getProblemsFrom(int firstRecordNumber, int size) {
