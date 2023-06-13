6197840668cbc0aa67446354302d2b39a6b8bd14
Rail Munirov <rail-ko@mail.ru>
2023-06-13 19:20:59 +0300

    menu

--- frontend/src/components/legacy/CheckupMenu/index.tsx
+++ frontend/src/components/legacy/CheckupMenu/index.tsx
@@ -1,16 +1,14 @@
+import { useTranslation } from "react-i18next";
 import styles from "./index.module.css";
 
 export const CheckupMenu = () => {
+  const { t } = useTranslation();
+
   return (
     <ul className={styles.root}>
       <li>
-        <a className={styles.link} href="#tasks">
-          Задания
-        </a>
-      </li>
-      <li>
-        <a className={styles.link} href="#extension">
-          Extension
+        <a className={styles.link} href="/admin">
+          {t("menu.admin")}
         </a>
       </li>
     </ul>
